faf4dff5bb5602301626c5083790c6725e688db9
[ - ( Ferly Afriliyan - Ryougaa) ] - <122451961+ferlyafriliyan@users.noreply.github.com>
2023-09-15 05:54:23 +0700

    Add files via upload

--- Try-Lzma/tes.py
+++ Try-Lzma/tes.py
@@ -12,13 +12,13 @@ import bz2
 import binascii
 import zlib
 
-
 mlgb_bz = f"""
             ╔╦╗┬  ┌─┐┌┐     ╔╗ ┌─┐
             ║║║│  │ ┬├┴┐    ╠╩╗┌─┘
             ╩ ╩┴─┘└─┘└─┘────╚═╝└─┘
             [ Marszhal Lzma Gzip Bz2 Binascii Zlib]
 """
+
 def prett(text):
     return text.title().center(os.get_terminal_size().columns)
 
@@ -39,8 +39,29 @@ def main():
     print("\n" + prett("[+] Try-Lzma Python File Encryptor [+]\n"))
 
     input_file = input("\t\tMasukan Nama File Input, Contoh : 'tes.py' : ")
+
+    # Periksa ekstensi file
+    if not input_file.endswith('.py'):
+        print("[•] File harus memiliki ekstensi .py")
+        exit()
+
+    # Periksa apakah file ada
+    if not os.path.exists(input_file):
+        print("[•] File tidak ditemukan")
+        exit()
+
     output_file = input("\t\tMasukkan Nama File Output, Contoh : 'tes_enc.py' : ")
+
+    # Periksa ekstensi output file
+    if not output_file.endswith('.py'):
+        print("[•] Output file harus memiliki ekstensi .py")
+        exit()
+
     complexity = int(input("\t\tFor Loop, Batasan : '100' (ex : 100): "))
+
+    if complexity > 100:
+        print("[•] Batas For Loop Hanya 100")
+        exit()
 
     print("\n" + prett("[+] Encrypting... [+]\n"))
 
