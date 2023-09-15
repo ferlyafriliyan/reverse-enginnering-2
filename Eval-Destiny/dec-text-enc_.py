c3b924ef57102f8cf42e07b6b5a29d217db0d55a
[ - ( Ferly Afriliyan - Ryougaa) ] - <122451961+ferlyafriliyan@users.noreply.github.com>
2023-09-16 02:25:46 +0700

    Delete Eval-Destiny/dec-text-enc_.py

--- Eval-Destiny/dec-text-enc_.py
+++
@@ -1,8 +0,0 @@
-def decrypt_random_encryption(encryption):
-    hex_values = encryption.split('\\x')[1:]
-    decrypted_text = ''.join([chr(int(hex_value, 16)) for hex_value in hex_values])
-    return decrypted_text
-
-encrypted_text = "contoh dari : text yang pengen di enc make simbol\\ "  # Ganti dengan nilai enkripsi yang sesuai
-decrypted_text = decrypt_random_encryption(encrypted_text)
-print(decrypted_text)
