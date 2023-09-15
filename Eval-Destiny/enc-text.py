5646a77d29b4684029f96ad9361d34bd6acad31e
[ - ( Ferly Afriliyan - Ryougaa) ] - <122451961+ferlyafriliyan@users.noreply.github.com>
2023-09-16 02:25:58 +0700

    Delete Eval-Destiny/enc-text.py

--- Eval-Destiny/enc-text.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def generate_random_encryption(target_text):
-    encryption = ''.join([f'\\x{ord(c):02x}' for c in target_text])
-    return encryption
-
-target_text = "text yang pengen di enc make simbol\\"
-encryption = generate_random_encryption(target_text)
-print(encryption)
