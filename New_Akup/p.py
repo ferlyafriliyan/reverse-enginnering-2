c8bef555fa1966ae419c714f645fb41f1eddba9e
[ - ( Ferly Afriliyan - Ryougaa) ] - <122451961+ferlyafriliyan@users.noreply.github.com>
2023-09-18 03:28:18 +0700

    Create p.py

---
+++ New_Akup/p.py
@@ -0,0 +1,6 @@
+# Akun - 1
+Nama = "Denventa Denventa"
+Email = "elq83199@nezid.com"
+TTL = "04/04/2000"
+Pass = "Ferly4409_$XD"
+print("[•] Akun - 1\nNama FB :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
