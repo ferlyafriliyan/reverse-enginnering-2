badc1a0a52b8babaaaeb57c84f4eed1df5d0c37b
[ - ( Ferly Afriliyan - Ryougaa) ] - <122451961+ferlyafriliyan@users.noreply.github.com>
2023-09-19 06:24:58 +0700

    Update f.py

--- New_Akun/f.py
+++ New_Akun/f.py
@@ -1,8 +1,23 @@
 import os as auto
 auto.system("clear")
+
 # Akun - 1
 Nama = "Denventa Denventa"
 Email = "elq83199@nezid.com"
 TTL = "04/04/2000"
 Pass = "Ferly4409_$XD"
+print("[•] Akun - 1\nNama FB :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun 2
+Nama = "Denventa Dev"
+Email = "cyr41869@omeie.com"
+TTL = "04/04/2000"
+Pass = "Ferly4409_$XD"
+print("[•] Akun - 1\nNama FB :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun 3
+Nama = "Denventa Dev"
+Email = "ber66338@zbok.com"
+TTL = "04/04/2000"
+Pass = " Ferly4409_$XD
 print("[•] Akun - 1\nNama FB :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
