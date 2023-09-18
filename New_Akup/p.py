ec66143609ff03d411290e1c8f449d0fc24505b8
[ - ( Ferly Afriliyan - Ryougaa) ] - <122451961+ferlyafriliyan@users.noreply.github.com>
2023-09-18 18:48:19 +0700

    Delete New_Akup directory

--- New_Akup/p.py
+++
@@ -1,8 +0,0 @@
-import os as auto
-auto.system("clear")
-# Akun - 1
-Nama = "Denventa Denventa"
-Email = "elq83199@nezid.com"
-TTL = "04/04/2000"
-Pass = "Ferly4409_$XD"
-print("[•] Akun - 1\nNama FB :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
