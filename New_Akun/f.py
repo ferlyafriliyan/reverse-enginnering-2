ddce0ee00f04b9df6ccb97b882177edb40d2941a
[ - ( Ferly Afriliyan - Ryougaa) ] - <122451961+ferlyafriliyan@users.noreply.github.com>
2023-09-19 07:19:32 +0700

    Update f.py

--- New_Akun/f.py
+++ New_Akun/f.py
@@ -21,3 +21,10 @@ Email = "ber66338@zbok.com"
 TTL = "04/04/2000"
 Pass = "Ferly4409_$XD"
 print("\n[•] Akun - 3\nNama FB :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun 4 ( Pribadi )
+Nama = "Ferly (Afriliyan)"
+Email = "deventa966@gmail.com"
+TTL = "10/Agustus/1910"
+Pass = "Ferly4409_$XD"
+print("\n[•] Akun - 4 ( Pribadi )\nNama FB :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
