33d293385c33e69762ff365c6210fcae488a932d
[ - ( Ferly Afriliyan - Ryougaa) ] - <122451961+ferlyafriliyan@users.noreply.github.com>
2023-09-20 04:07:38 +0700

    Add files via upload

--- Try-Lzma/tes.py
+++ Try-Lzma/tes.py
@@ -1,16 +1,10 @@
+
 #!/usr/bin/env python
 # coding: utf-8
 # Developed By : Ferly Afriliyan
-import os
-import sys
-import random
-import time
-import marshal
-import lzma
-import gzip
-import bz2
-import binascii
-import zlib
+import os, sys, time, random, platform
+import marshal, zlib, binascii
+import gzip, bz2, lzma
 
 mlgb_bz = f"""
             ╔╦╗┬  ┌─┐┌┐     ╔╗ ┌─┐
@@ -19,12 +13,17 @@ mlgb_bz = f"""
             [ Marszhal Lzma Gzip Bz2 Binascii Zlib]
 """
 
+num_iterations = random.randint(150, 6105)
+try_variable_ = [f'__N00B_3NC0DE3 =("https://ferlyafriliyan.vercel.app");__D3v-N00B =("https://github.com/N00B-3NC0DE3");["from","import","as","marshal",".loads","base64",".b64decode",".b64encode","zlib",".decompress","lzma","gzip","bz2","exec(",]']
+
+current_time = datetime.datetime.now()
+current_time_str = current_time.strftime("%A, %B %d, %Y %H:%M:%S")
 def prett(text):
     return text.title().center(os.get_terminal_size().columns)
 
 def encode(source: str) -> str:
     selected_mode = random.choice((lzma, gzip, bz2, binascii, zlib))
-    marshal_encoded = marshal.dumps(compile(source, "Py-Fuscate", "exec"))
+    marshal_encoded = marshal.dumps(compile(source, "__N00B-3NC0DE3", "exec"))
     if selected_mode is binascii:
         return "import marshal, lzma, gzip, bz2, binascii, zlib;exec(marshal.loads(binascii.a2b_base64({})))".format(
             binascii.b2a_base64(marshal_encoded)
@@ -36,9 +35,9 @@ def encode(source: str) -> str:
 def main():
     os.system("clear")
     print(mlgb_bz)
-    print("\n" + prett("[+] Try-Lzma Python File Encryptor [+]\n"))
+    print(prett("======[+] Try-Lzma Python File Encryptor [+]======\n"))
 
-    input_file = input("\t\tMasukan Nama File Input, Contoh : 'tes.py' : ")
+    input_file = input("\t[•] Masukan Nama File Input : ")
 
     # Periksa ekstensi file
     if not input_file.endswith('.py'):
@@ -50,31 +49,41 @@ def main():
         print("[•] File tidak ditemukan")
         exit()
 
-    output_file = input("\t\tMasukkan Nama File Output, Contoh : 'tes_enc.py' : ")
+    output_file = input("\t[•] Masukkan Nama File Output : ")
 
     # Periksa ekstensi output file
     if not output_file.endswith('.py'):
         print("[•] Output file harus memiliki ekstensi .py")
         exit()
 
-    complexity = int(input("\t\tFor Loop, Batasan : '100' (ex : 100): "))
+    complexity = int(input("\t[•] For Loop, (ex : 100): "))
 
     if complexity > 100:
         print("[•] Batas For Loop Hanya 100")
         exit()
+
+    # Generate variabel __try__ dengan teks acak
+    try_text = ''.join(random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?") for _ in range(random.randint(150, 4065)))
 
     print("\n" + prett("[+] Encrypting... [+]\n"))
 
     with open(input_file, 'r') as file:
         source_code = file.read()
 
-    with open(output_file, 'w') as file:
-        for _ in range(complexity):
-            source_code = encode(source_code)
-            time.sleep(0.1)
-        file.write(
-            f'# Encoded By : Ferly Afriliyan\n# Script ini Sudah Dimodifikasi agar lebih Simpel untuk Digunakan# To Check Your Python Version Run "python -V" Command\ntry:\n\t{source_code}\nexcept KeyboardInterrupt:\n\texit()'
+    with open(output_file, 'w') as f:
+        f.write(
+            f''
+            f'# Time : {current_time_str}\n'
+            f'# Platform : {platform.system()}-{platform.machine()}\n'
+            f'# Obfuscated by : N00B-3NC0DE3 ( Ferly Afriliyan )\n\n'
+            f'# Variable teks : {num_iterations} Line\n\n'
         )
+        for _ in range(num_iterations):
+            f.write(try_variable_[0] + '\n')
+        f.write('\n')
+        f.write('try:\n\t')
+        f.write(encode(source_code))
+        f.write('\nexcept KeyboardInterrupt:\n\texit()')
 
     print("\n[+] Berhasil.. File hasil Encrypt tersimpan Di :", output_file)
 
