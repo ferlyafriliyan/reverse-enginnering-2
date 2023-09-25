c5eaf419bcf3c9d4116485960edf0317ffd04afb
[ - ( Ferly Afriliyan - Ryougaa) ] - <122451961+ferlyafriliyan@users.noreply.github.com>
2023-09-26 06:52:22 +0700

    Create email.py

---
+++ New_Akun/email.py
@@ -0,0 +1,30 @@
+import os as auto
+auto.system("clear")
+
+# Akun - 1
+Nama = "Denventa Ferly"
+Email = "denventaferly@gmail.com"
+TTL = "04/04/2000"
+Pass = "Ferly4409#"
+print("\n[•] Akun - 1\nNama Email :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun - 2
+Nama = "Ferly Afrliyn"
+Email = "ferlyafrliyn@gmail.com"
+TTL = "04/04/2000"
+Pass = "Ferly4409#"
+print("\n[•] Akun - 2\nNama Email :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun - 3
+Nama = "Denventa"
+Email = "deventa966@gmail.com"
+TTL = "04/04/2000"
+Pass = "Ferly4409#"
+print("\n[•] Akun - 3\nNama Email :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun - 4
+Nama = "Ferly Denventa"
+Email = "devenfer449@gmail.com"
+TTL = "04/04/2000"
+Pass = "Ferly4409#"
+print("\n[•] Akun - 4\nNama Email :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
