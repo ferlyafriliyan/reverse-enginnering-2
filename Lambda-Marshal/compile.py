567e0758114ef52f04744f2734a67c84c873cc2a
[ - ( Ferly Afriliyan - Ryougaa) ] - <122451961+ferlyafriliyan@users.noreply.github.com>
2023-09-29 04:13:56 +0700

    Add files via upload

--- Lambda-Marshal/compile.py
+++ Lambda-Marshal/compile.py
@@ -1,10 +1,15 @@
 import marshal
 import base64
 import zlib
-import time
+import time;import datetime
+import platform;from random import randint as rr
 from os import system as autoclear
-autoclear('clear')
 
+variable_value = '=(:__:-_= __, lambda c; _ = ,__ .' * rr(14, 44)
+
+current_time = datetime.datetime.now()
+current_time_str = current_time.strftime("%A, %B %d, %Y %H:%M:%S")
+            
 try:
     file = input("Input File : ")
     total = int(input("Limit : "))
@@ -15,11 +20,18 @@ try:
 
         # Metode Enkripsi
         encoded_data = base64.b64encode(zlib.compress(marshal.dumps(compile(xos, '', 'exec')))).decode()
-
+            
         with open(out, 'wb') as w:
             w.write(b"import marshal\n")
             w.write(b"import base64\n")
             w.write(b"import zlib\n")
+            w.write(b"\n")
+            w.write(f"# Time : {current_time_str}\n".encode())
+            w.write(f"# Platform : {platform.system()}-{platform.machine()}\n".encode())
+            w.write("# Obfuscated by : Ferly Afriliyan (Ryougaa Hideki)\n".encode())
+            w.write(b"\n")
+            w.write(f"# Variable Value : {len(encoded_data)}\n".encode())
+            w.write(b"\n")
             w.write(f"exec((lambda _ : (marshal.loads(zlib.decompress(base64.b64decode(_))) if _ else None))('{encoded_data}'))\n".encode())
 
         komter = 0
@@ -30,13 +42,20 @@ try:
                 w.write(b"import marshal\n")
                 w.write(b"import base64\n")
                 w.write(b"import zlib\n")
+                w.write(b"\n")
+                w.write(f"# Time : {current_time_str}\n".encode())
+                w.write(f"# Platform : {platform.system()}-{platform.machine()}\n".encode())
+                w.write("# Obfuscated by : Ferly Afriliyan (Ryougaa Hideki)\n".encode())
+                w.write(b"\n")
+                w.write(f"# Variable Value : {len(encoded_data)}\n".encode())
+                w.write(b"\n")
                 w.write(f"exec((lambda _ : (marshal.loads(zlib.decompress(base64.b64decode(_))) if _ else None))('{encoded_data}'))\n".encode())
 
             current_iteration = komter
             total_iterations = total
             loading_percentage = (current_iteration / total_iterations) * 100
             print(f"Encrypting: [{loading_percentage:.2f}%]", end='\r')
-            time.sleep(0.1)
+            time.sleep(0.00)
             komter += 1
 
         compiled_file = file.replace('.py', '_.py')
