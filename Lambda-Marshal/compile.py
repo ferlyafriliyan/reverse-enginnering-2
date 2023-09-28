f49be1df8eb4df3a343b92eef66bf4bca9125278
[ - ( Ferly Afriliyan - Ryougaa) ] - <122451961+ferlyafriliyan@users.noreply.github.com>
2023-09-29 02:33:20 +0700

    Add files via upload

--- Lambda-Marshal/compile.py
+++ Lambda-Marshal/compile.py
@@ -1,15 +1,18 @@
-import sys, argparse, marshal, zlib, base64, codecs, binascii
+import marshal
+from os import system as autoclear
+autoclear('clear')
+
 
 try:
     file = input("Input File : ")
     total = int(input("Limit : "))
-    
+
     if total < 101:
         out = input("Output File : ")
         xos = open(file).read()
         
         # Metode Marshal
-        encoded_marshal = repr(marshal.dumps(xos))
+        encoded_marshal = repr(marshal.dumps(compile(xos, '', 'exec')))
         
         with open(out, 'w') as w:
             w.write(f"import marshal\n")
@@ -17,14 +20,14 @@ try:
             
         komter = 0
         while total >= komter:
-            encoded_marshal = repr(marshal.dumps(open(out).read()))
+            encoded_marshal = repr(marshal.dumps(compile(open(out).read(), '', 'exec')))
             with open(out, 'w') as w:
                 w.write(f"import marshal\n")
                 w.write(f"exec((lambda _ : (marshal.loads(_) if _ else None))({encoded_marshal}))")
             komter += 1
             print(f"\nSuccess Obfuscate File, File saved to : {out}")
             
-        compiled_file = file.replace('.py', '_compiled.py')
+        compiled_file = file.replace('.py', '_.py')
         with open(compiled_file, 'wb') as compiled:
             compiled.write(marshal.dumps(compile(open(out).read(), '', 'exec')))
         print(f"Compiled file saved to: {compiled_file}")
