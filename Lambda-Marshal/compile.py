68414ec4e696de9418587e7177561cfc1c739bcd
[ - ( Ferly Afriliyan - Ryougaa) ] - <122451961+ferlyafriliyan@users.noreply.github.com>
2023-09-29 02:50:13 +0700

    Add files via upload

--- Lambda-Marshal/compile.py
+++ Lambda-Marshal/compile.py
@@ -2,7 +2,6 @@ import marshal
 from os import system as autoclear
 autoclear('clear')
 
-
 try:
     file = input("Input File : ")
     total = int(input("Limit : "))
@@ -31,6 +30,9 @@ try:
         with open(compiled_file, 'wb') as compiled:
             compiled.write(marshal.dumps(compile(open(out).read(), '', 'exec')))
         print(f"Compiled file saved to: {compiled_file}")
+        
+        # Evaluasi kode yang telah di-compile
+        eval(compile(open(compiled_file).read(), '', 'exec'))
             
         exit(f"Limit Max > {total} 101")
 except Exception as e:
