e85ba8d5ef6facbd37cb57c548929e4025ff768b
Ferly Afriliyan - Ryougaa <122451961+ferlyafriliyan@users.noreply.github.com>
2023-10-23 23:09:01 +0700

    Add files via upload

--- pycryptodome/main.py
+++ pycryptodome/main.py
@@ -2,14 +2,24 @@ import importlib
 import requests as r
 from bs4 import BeautifulSoup as bs
 import os, sys
-
+# •
 import datetime, time
 import platform
+
+k= '\033[1;33m'; a = '\033[1;30m'; m= '\033[1;31m'; h = '\033[1;32m'; p = '\033[1;37m';
 
 # Clear Terminal
 def clear():
     if 'linux' in sys.platform.lower():os.system('clear')
     elif 'win' in sys.platform.lower():os.system('cls')
+
+# Memeriksa apakah modul requests terinstall
+try:
+    clear()
+    importlib.import_module('requests')
+    requests_module_installed = True
+except ImportError:
+    requests_module_installed = False
 
 # Memeriksa apakah modul Crypto (pycryptodome) terinstall
 try:
@@ -20,15 +30,8 @@ except ImportError:
     crypto_module_installed = False
 
 if not crypto_module_installed:
-    print(f"Modul Crypto   : (pycryptodome) belum terinstall. Anda perlu menginstal modul ini sebelum menggunakan program ini.")
-    print(f"Ketik perintah : pip install pycryptodome")
-
-# Memeriksa apakah modul requests terinstall
-try:
-    importlib.import_module('requests')
-    requests_module_installed = True
-except ImportError:
-    requests_module_installed = False
+    print(f"{h}[{a}•{h}] {p}Modul Crypto   : ({a}pycryptodome{p}) belum terinstall.")
+    print(f"{h}[{a}•{h}] {p}Ketik perintah : pip install {a}pycryptodome")
 
 # Memeriksa apakah modul bs4 terinstall
 try:
@@ -38,12 +41,12 @@ except ImportError:
     bs4_module_installed = False
 
 if not crypto_module_installed or not requests_module_installed or not bs4_module_installed:
-    print("Beberapa modul belum terinstal. Anda perlu menginstal modul-modul berikut:")
-    if not crypto_module_installed:
-        print("  - pycryptodome (Crypto)")
-        print("  - requests")
-        print("  - bs4 (Beautiful Soup)")
-    print("Ketik perintah: pip install pycryptodome bs4 requests")
+    print(f"{h}[{a}•{h}] {p}Beberapa modul belum terinstal. Anda perlu menginstal modul-modul berikut {a}:")
+    if not requests_module_installed:
+        print(f"  {a}- {p}pycryptodome (Crypto)")
+        print(f"  {a}- {p}requests")
+        print(f"  {a}- {p}bs4 (Beautiful Soup)")
+    print(f"{h}[{a}•{h}] {p}Ketik perintah: pip install {a}pycryptodome bs4 requests {p}")
 else:
     # Clear Terminal
     if 'linux' in platform.system().lower():os.system('clear')
@@ -54,14 +57,14 @@ else:
     current_time_str = current_time.strftime("%A, %B %d, %Y %H:%M:%S")
 
     # Meminta pengguna untuk memasukkan nama file input
-    input_file = input("Masukkan nama file Input: ")
+    input_file = input(f"{h}[{a}•{h}] {p}Masukkan nama file Input {a}: {p}")
 
     # Membaca teks dari file input
     try:
         with open(input_file, "r", encoding="utf-8") as file:
             input_text = file.read()
     except FileNotFoundError:
-        print(f"File '{input_file}' tidak ditemukan.")
+        print(f"{p}File '{input_file}' tidak ditemukan.")
         input_text = None
 
     # Jika teks input berhasil dibaca, maka lakukan proses enkripsi
@@ -77,12 +80,12 @@ else:
 
         # Meminta pengguna untuk memasukkan nama file output
         while True:
-            output_file = input("Masukkan nama file Output: ")
+            output_file = input(f"{h}[{a}•{h}] {p}Masukkan nama file Output {a}: {p}")
             if not output_file:
-                print("Isi dengan benar!")
+                print(f"{m}[{a}!{m}] {p}Isi dengan benar {m}! {p}")
                 exit()
             elif not output_file.endswith(".py"):
-                print("File output harus memiliki ekstensi .py")
+                print(f"{m}[{a}!{m}] {p}File output harus memiliki ekstensi .py {p}")
                 exit()
             else:
                 break
@@ -95,8 +98,8 @@ else:
                 file.write(f'created_by = "Ferly Afriliyan and Merch"\n')
                 file.write(f'# Time : {current_time_str}\n')
                 file.write(f'# Platform : {platform.system()}-{platform.machine()}\n' + encrypted_code)
-            print(f"Berhasil, file {input_file} tersimpan di: {output_file}")
+            print(f"{h}[{a}•{h}] {p}Berhasil, file {input_file} tersimpan di {a}: {output_file} {p}")
         else:
-            print("Tidak dapat menemukan kode Obfuscate.")
+            print(f"{m}[{a}!{m}] {p}Tidak dapat menemukan kode Obfuscate.")
     else:
-        print("Proses Obfuscate dibatalkan karena file Input tidak ditemukan.")
+        print(f"{h}[{k}+{h}] {p}Proses Obfuscate dibatalkan karena file Input tidak ditemukan.")
