40f93fbf064d5f6b43539363433f416e1985b42a
Ferly Afriliyan - Ryougaa <122451961+ferlyafriliyan@users.noreply.github.com>
2023-11-08 14:04:01 +0700

    Add files via upload

--- Hyperion/Hyperion.py
+++ Hyperion/Hyperion.py
@@ -56,38 +56,18 @@ class Hyperion():
 
     interpreterClass = """
 class Interpreter():
-    def __init__(self, code: str, layersFunction: bytes, module, globals_, backend: bytes = b'') -> None:
-        self.__module = module
-        self.layersFunction = layersFunction
-        self.__globals = globals_
-        self.code = {'bytes': code, 'str': str(code)}
-        self.__backend = backend
-
-    def __tunnel(self) -> Gateway:
-        return Gateway(self.__backend, GATEWAYKEY, __module=self.__module, __globals=self.__globals, interpreter=self)
-
-    def Run(self) -> None:
-        decoder = self.__getobject__()
-        gate = self.__tunnel().Pass()
-        exec(eval(marshal.loads(decoder),
-                  {'__selfObject__': self, '__module': self.__module, '__sr_m': MARSHALMODULE, '__globals': self.__globals, 'gate': gate}),
-                  self.__globals)
+    def __init__(self, code: str, layersFunction: bytes, module, globals_, backend: bytes = b'') -> None: self.__module = module;self.layersFunction = layersFunction;self.__globals = globals_;self.code = {'bytes': code, 'str': str(code)};self.__backend = backend
+    def __tunnel(self) -> Gateway: return Gateway(self.__backend, 7605, __module = self.__module, __globals = self.__globals, interpreter = self)
+    def Run(self) -> None: decoder = self.__getobject__(); gate = self.__tunnel().Pass();exec(eval(marshal.loads(decoder), {'__selfObject__': self, '__module': self.__module, '__sr_m': MARSHALMODULE, '__globals': self.__globals, 'gate': gate}), self.__globals)
 
     def __getobject__(self) -> object:
         func = self.layersFunction
         return self.__module.b64decode(func)
-    """[1:-1].replace('MARSHALMODULE', Alt('__import__("marshal")')).replace('GATEWAYKEY', str(gatewayKey))
+        """[1:-1].replace('MARSHALMODULE', Alt('__import__("marshal")')).replace('GATEWAYKEY', str(gatewayKey))
 
     gatewayClass = """
 class Gateway():
-    def __init__(self, way: bytes, key: int, **ext) -> None:
-        self.way = way
-        self.key = key
-        self.module__ = ext.get('__module', None)
-        self.__globals = ext.get('__globals', None)
-        self.__module = ext.get('__module', None)
-        self.__interpreter = ext.get('interpreter', None)
-
+    def __init__(self, way: bytes, key: int, **ext) -> None: self.way = way;self.key = key; self.module__ = ext.get('__module', None);self.__globals = ext.get('__globals', None);self.__module = ext.get('__module', None); self.__interpreter = ext.get('interpreter', None)
     def Pass(self):
         exec(marshal.loads(self.module__.b16decode(self.way)),
              {'__selfObject__': self, '__key__': self.key, '__module': self.module__, '__globals': self.__globals, '__InterpreterObject__': self.__interpreter})
