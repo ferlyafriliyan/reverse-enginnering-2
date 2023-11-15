813bf826d8cde74351904e9e88fd669c35f45d92
Ferly Afriliyan - Ryougaa <122451961+ferlyafriliyan@users.noreply.github.com>
2023-11-15 23:11:15 +0700

    Add files via upload

---
+++ pyarmor/pyarmor.py
@@ -0,0 +1,96 @@
+import subprocess
+import os
+
+k = '\033[1;33m'  # Warna Kuning
+a = '\033[1;30m'  # Warna Hitam/Abu-Abu
+m = '\033[1;31m'  # Warna Merah
+h = '\033[1;32m'  # Warna Hijau
+p = '\033[1;37m'  # Warna Putih
+b = '\033[1;34m'  # Warna Biru
+v = '\033[1;35m'  # Warna Violet
+u = '\033[1;36m'  # Warna Ungu
+j = '\033[1;38;5;202m'  # Warna Jingga
+
+# Logo
+Pyarmor = r"""
+
+█ ▄▄ ▀▄    ▄ ██   █▄▄▄▄ █▀▄▀█ ████▄ █▄▄▄▄ 
+█   █  █  █  █ █  █  ▄▀ █ █ █ █   █ █  ▄▀ 
+█▀▀▀    ▀█   █▄▄█ █▀▀▌  █ ▄ █ █   █ █▀▀▌  
+█       █    █  █ █  █  █   █ ▀████ █  █  
+ █    ▄▀        █   █      █          █   
+  ▀            █   ▀      ▀          ▀    
+              ▀                           """
+def check_module(module_name, install_command):
+    try:
+        __import__(module_name)
+        return True
+    except ImportError:
+        print(f"{h}[{m}•{h}] {p}Error: Module {a}{module_name} {p}not found.")
+        print(f"{h}[{m}•{h}] {p}To install, run: {a}{install_command} {p}")
+        return False
+
+def obfuscate_file(input_file):
+    if not check_module("pyarmor", "pip3 install pyarmor"):
+        return
+    
+    # Periksa apakah perintah 'pyarmor' dapat dijalankan di terminal
+    try:
+        subprocess.run("pyarmor", shell=True, check=True)
+    except subprocess.CalledProcessError:
+        print(f"{h}[{m}•{h}] {p}Error: 'pyarmor' is not recognized as an internal or external command.")
+        print(f"{h}[{m}•{h}] {p}Make sure 'pyarmor' is installed and accessible in the terminal.")
+        return
+
+    try:
+        # Mengeksekusi perintah PyArmor untuk menghasilkan versi obfuscated
+        result = subprocess.run(f"pyarmor gen {input_file}", shell=True, check=True, capture_output=True, text=True)
+        
+        # Mengganti header dengan string kosong
+        modified_output = result.stdout.replace(
+            "INFO     Python 3.11.6\nINFO     Pyarmor 8.4.3 (trial), 000000, non-profits\nINFO     Platform windows.x86_64\n", "")
+
+        # Mencetak informasi proses PyArmor
+        print(modified_output)
+        print(f"""INFO     Python 3.11.6
+INFO     Pyarmor 8.4.3 (trial), 000000, non-profits
+INFO     Platform windows.x86_64
+INFO     search inputs ...
+INFO     find script pyarmor.py
+INFO     find 1 top resources
+INFO     start to generate runtime files
+INFO     target platforms {'windows.amd64'}
+INFO     write dist\pyarmor_runtime_000000\pyarmor_runtime.pyd
+INFO     generate runtime files OK
+INFO     start to obfuscate scripts
+INFO     process resource "pyarmor"
+INFO     obfuscating file pyarmor.py
+INFO     write dist\pyarmor.py
+INFO     obfuscate scripts OK
+""")
+
+        # Mencetak informasi obfuscation
+        print(f"{h}[{m}•{h}] {p}Berhasil menghasilkan versi obfuscated: {a}dist\{input_file} {p}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    os.system("cls")
+    print(Pyarmor)
+    if not check_module("pyarmor", "pip install pyarmor"):
+        exit(1)
+
+    # User file input
+    input_file = input(f"{h}[{a}•{h}] {p}Input file {a}: {p}")
+    if not input_file:
+        print(f"└──>{m}[{a}!{m}] {p}File '{input_file}' tidak ditemukan.")
+        exit()
+    elif not input_file.endswith(".py"):
+        print(f"└──>{m}[{a}!{m}] {p}File harus memiliki ekstensi .py {p}")
+        exit()
+    # Pastikan file yang dimasukkan oleh pengguna ada
+    if not os.path.isfile(input_file):
+        print(f"File '{input_file}' tidak ditemukan.")
+    else:
+        # Menjalankan obfuscation
+        obfuscate_file(input_file)
