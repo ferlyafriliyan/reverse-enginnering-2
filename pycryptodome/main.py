8f469916f11e28fd764a828d67af9ab5d1ae3a2c
Ferly Afriliyan - Ryougaa <122451961+ferlyafriliyan@users.noreply.github.com>
2023-11-30 14:29:56 +0700

    Update main.py

--- pycryptodome/main.py
+++ pycryptodome/main.py
@@ -2,36 +2,42 @@ import importlib
 import requests as r
 from bs4 import BeautifulSoup as bs
 import os, sys
+
 # •
 import datetime, time
 import platform
 
-k = '\033[1;33m'  # Warna Kuning
-a = '\033[1;30m'  # Warna Hitam/Abu-Abu
-m = '\033[1;31m'  # Warna Merah
-h = '\033[1;32m'  # Warna Hijau
-p = '\033[1;37m'  # Warna Putih
-b = '\033[1;34m'  # Warna Biru
-v = '\033[1;35m'  # Warna Violet
-u = '\033[1;36m'  # Warna Ungu
-j = '\033[1;38;5;202m'  # Warna Jingga
+k = "\033[1;33m"  # Warna Kuning
+a = "\033[1;30m"  # Warna Hitam/Abu-Abu
+m = "\033[1;31m"  # Warna Merah
+h = "\033[1;32m"  # Warna Hijau
+p = "\033[1;37m"  # Warna Putih
+b = "\033[1;34m"  # Warna Biru
+v = "\033[1;35m"  # Warna Violet
+u = "\033[1;36m"  # Warna Ungu
+j = "\033[1;38;5;202m"  # Warna Jingga
 
 # Clear Terminal
 def clear():
-    if 'linux' in sys.platform.lower():os.system('clear')
-    elif 'win' in sys.platform.lower():os.system('cls')
+    if "linux" in sys.platform.lower():
+        os.system("clear")
+    elif "win" in sys.platform.lower():
+        os.system("cls")
+
 
 logo = r"""
 ╔═╗┬ ┬┌─┐┬─┐┬ ┬┌─┐┌┬┐┌─┐┌┬┐┌─┐┌┬┐┌─┐
 ╠═╝└┬┘│  ├┬┘└┬┘├─┘ │ │ │ │││ ││││├┤  | Coded by : Ferly Afriliyan and Merch
 ╩   ┴ └─┘┴└─ ┴ ┴   ┴ └─┘─┴┘└─┘┴ ┴└─┘
 [•] obfuscate python code with methods that are quite difficult to reverse
-"""[:-1]
+"""[
+    :-1
+]
 
 # Memeriksa apakah modul requests terinstall
 try:
     clear()
-    importlib.import_module('requests')
+    importlib.import_module("requests")
     requests_module_installed = True
 except ImportError:
     requests_module_installed = False
@@ -39,7 +45,7 @@ except ImportError:
 # Memeriksa apakah modul Crypto (pycryptodome) terinstall
 try:
     clear()
-    importlib.import_module('Crypto')
+    importlib.import_module("Crypto")
     crypto_module_installed = True
 except ImportError:
     crypto_module_installed = False
@@ -50,22 +56,32 @@ if not crypto_module_installed:
 
 # Memeriksa apakah modul bs4 terinstall
 try:
-    importlib.import_module('bs4')
+    importlib.import_module("bs4")
     bs4_module_installed = True
 except ImportError:
     bs4_module_installed = False
 
-if not crypto_module_installed or not requests_module_installed or not bs4_module_installed:
-    print(f"{h}[{a}•{h}] {p}Beberapa modul belum terinstal. Anda perlu menginstal modul-modul berikut {a}:")
+if (
+    not crypto_module_installed
+    or not requests_module_installed
+    or not bs4_module_installed
+):
+    print(
+        f"{h}[{a}•{h}] {p}Beberapa modul belum terinstal. Anda perlu menginstal modul-modul berikut {a}:"
+    )
     if not requests_module_installed:
         print(f"  {a}- {p}pycryptodome (Crypto)")
         print(f"  {a}- {p}requests")
         print(f"  {a}- {p}bs4 (Beautiful Soup)")
-    print(f"{h}[{a}•{h}] {p}Ketik perintah: pip install {a}pycryptodome bs4 requests {p}")
+    print(
+        f"{h}[{a}•{h}] {p}Ketik perintah: pip install {a}pycryptodome bs4 requests {p}"
+    )
 else:
     # Clear Terminal
-    if 'linux' in platform.system().lower():os.system('clear')
-    elif 'win' in platform.system().lower():os.system('cls')
+    if "linux" in platform.system().lower():
+        os.system("clear")
+    elif "win" in platform.system().lower():
+        os.system("cls")
 
     Take_the_encryption = "https://pyobfuscate.com/pyd"
     current_time = datetime.datetime.now()
@@ -94,13 +110,15 @@ else:
     # Jika teks input berhasil dibaca, maka lakukan proses enkripsi
     if input_text:
         # Kirim permintaan POST
-        response = r.post('https://pyobfuscate.com/pyd', data={'input_text': input_text})
+        response = r.post(
+            "https://pyobfuscate.com/pyd", data={"input_text": input_text}
+        )
 
         # Parsing hasil respons
-        source = bs(response.text, 'html.parser')
+        source = bs(response.text, "html.parser")
 
         # Cari elemen textarea dengan id 'myTextarea2'
-        result = source.find('textarea', {'id': 'myTextarea2'})
+        result = source.find("textarea", {"id": "myTextarea2"})
 
         # Meminta pengguna untuk memasukkan nama file output
         while True:
@@ -118,12 +136,21 @@ else:
         if result:
             encrypted_code = result.text
             with open(output_file, "w", encoding="utf-8") as file:
-                file.write(f'# Take the encryption method from the website = {Take_the_encryption}\n')
+                file.write(
+                    f"# Take the encryption method from the website = {Take_the_encryption}\n"
+                )
                 file.write(f'created_by = "Ferly Afriliyan and Merch"\n')
-                file.write(f'# Time : {current_time_str}\n')
-                file.write(f'# Platform : {platform.system()}-{platform.machine()}\n' + encrypted_code)
-            print(f"{h}[{a}•{h}] {p}Berhasil, file {input_file} tersimpan di {a}: {output_file} {p}")
+                file.write(f"# Time : {current_time_str}\n")
+                file.write(
+                    f"# Platform : {platform.system()}-{platform.machine()}\n"
+                    + encrypted_code
+                )
+            print(
+                f"{h}[{a}•{h}] {p}Berhasil, file {input_file} tersimpan di {a}: {output_file} {p}"
+            )
         else:
             print(f"{m}[{a}!{m}] {p}Tidak dapat menemukan kode Obfuscate.")
     else:
-        print(f"{h}[{k}+{h}] {p}Proses Obfuscate dibatalkan karena file Input tidak ditemukan.")
+        print(
+            f"{h}[{k}+{h}] {p}Proses Obfuscate dibatalkan karena file Input tidak ditemukan."
+        )
