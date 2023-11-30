5dd0711d6a25c792c424045a6ecb1ae094a6dc70
Ferly Afriliyan - Ryougaa <122451961+ferlyafriliyan@users.noreply.github.com>
2023-11-30 23:16:39 +0700

    Add files via upload

--- PyPI-PyObfuscator/main.py
+++ PyPI-PyObfuscator/main.py
@@ -1,15 +1,22 @@
 import subprocess
-import os
+import os, sys
 
-k = '\033[1;33m'  # Warna Kuning
-a = '\033[1;30m'  # Warna Hitam/Abu-Abu
-m = '\033[1;31m'  # Warna Merah
-h = '\033[1;32m'  # Warna Hijau
-p = '\033[1;37m'  # Warna Putih
-b = '\033[1;34m'  # Warna Biru
-v = '\033[1;35m'  # Warna Violet
-u = '\033[1;36m'  # Warna Ungu
-j = '\033[1;38;5;202m'  # Warna Jingga
+# Clear Terminal
+def clear_terminal():
+    if "linux" in sys.platform.lower():
+        os.system("clear")
+    elif "win" in sys.platform.lower():
+        os.system("cls")
+        
+k = "\033[1;33m"  # Warna Kuning
+a = "\033[1;30m"  # Warna Hitam/Abu-Abu
+m = "\033[1;31m"  # Warna Merah
+h = "\033[1;32m"  # Warna Hijau
+p = "\033[1;37m"  # Warna Putih
+b = "\033[1;34m"  # Warna Biru
+v = "\033[1;35m"  # Warna Violet
+u = "\033[1;36m"  # Warna Ungu
+j = "\033[1;38;5;202m"  # Warna Jingga
 
 # Logo
 PyPI = r"""
@@ -22,6 +29,7 @@ PyPI = r"""
   ▀            ▀                        ▀    
                                              """
 
+
 def check_module(module_name, install_command):
     try:
         __import__(module_name)
@@ -30,6 +38,7 @@ def check_module(module_name, install_command):
         print(f"{h}[{a}•{h}] {p}Module : {a}{module_name} {p}belum terinstall.")
         print(f"{h}[{a}•{h}] {p}Ketik perintah : {a}{install_command} {p}")
         return False
+
 
 def obfuscate_file(input_file, output_file):
     # Periksa apakah modul PyObfuscator sudah terinstall
@@ -42,25 +51,34 @@ def obfuscate_file(input_file, output_file):
     # Menjalankan perintah menggunakan subprocess
     try:
         # Menangkap output dari subprocess
-        result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
-        
+        result = subprocess.run(
+            command, shell=True, check=True, capture_output=True, text=True
+        )
+
         # Mengganti header dengan string kosong
-        modified_output = result.stdout.replace("PyObfuscator  Copyright (C) 2021, 2022, 2023  Maurice Lambert\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder certain conditions.", "")
-        
+        modified_output = result.stdout.replace(
+            "PyObfuscator  Copyright (C) 2021, 2022, 2023  Maurice Lambert\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it\nunder certain conditions.",
+            "",
+        )
+
         # Menambahkan keterangan pada file hasil obfuscate
-        with open(output_file, 'a') as f:
-            f.write(f'\n')
+        with open(output_file, "a") as f:
+            f.write(f"\n")
             f.write("\n# Obfuscated by: Ferly Afriliyan\n")
-            f.write("# Encryption method: PyPI PyObfuscator\n")  # Gantilah dengan metode encrypt yang sesuai
+            f.write(
+                "# Encryption method: PyPI PyObfuscator\n"
+            )  # Gantilah dengan metode encrypt yang sesuai
 
         # Menampilkan output yang sudah dimodifikasi dengan pemisah baris
-        print(f"\n{h}[{a}•{h}] {p}Berhasil, file {input_file} tersimpan di {a}: {output_file} {p}{modified_output.strip()}")
+        print(
+            f"\n{h}[{a}•{h}] {p}Berhasil, file {input_file} tersimpan di {a}: {output_file} {p}{modified_output.strip()}"
+        )
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
 
 
 if __name__ == "__main__":
-    os.system("cls")
+    clear_terminal()
     print(PyPI)
     if not check_module("PyObfuscator", "pip install PyObfuscator"):
         exit(1)
