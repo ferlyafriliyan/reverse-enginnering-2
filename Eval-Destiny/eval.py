a6417780eccedfb219353cc1d87573ea1b95b3a2
Ferly Afriliyan - Ryougaa <122451961+ferlyafriliyan@users.noreply.github.com>
2023-12-17 01:28:25 +0700

    Add files via upload

--- Eval-Destiny/eval.py
+++ Eval-Destiny/eval.py
@@ -11,52 +11,59 @@ from sys import argv
 from rich import print
 
 # Definisi warna teks ANSI
-Hitam = "\x1b[0;90m"     # Hitam
-Merah = "\x1b[38;5;196m" # Merah
+Hitam = "\x1b[0;90m"  # Hitam
+Merah = "\x1b[38;5;196m"  # Merah
 Hijau = "\x1b[38;5;46m"  # Hijau
-Kuning = "\x1b[38;5;226m" # Kuning
+Kuning = "\x1b[38;5;226m"  # Kuning
 Biru = "\x1b[38;5;44m"  # Biru
-Ungu = "\x1b[0;95m"     # Ungu
-Putih = "\x1b[38;5;231m" # Putih
-Jingga = "\x1b[38;5;208m" # Jingga
-Abu = "\x1b[38;5;248m" # Abu-Abu
+Ungu = "\x1b[0;95m"  # Ungu
+Putih = "\x1b[38;5;231m"  # Putih
+Jingga = "\x1b[38;5;208m"  # Jingga
+Abu = "\x1b[38;5;248m"  # Abu-Abu
 
 # Definisi warna teks Rich
-Z2 = "[#000000]" # Hitam
-M2 = "[#FF0000]" # Merah
-H2 = "[#00FF00]" # Hijau
-K2 = "[#FFFF00]" # Kuning
-B2 = "[#00C8FF]" # Biru
-U2 = "[#AF00FF]" # Ungu
-N2 = "[#FF00FF]" # Pink
-O2 = "[#00FFFF]" # Biru Muda
-P2 = "[#FFFFFF]" # Putih
-J2 = "[#FF8F00]" # Jingga
-A2 = "[#AAAAAA]" # Abu-Abu
+Z2 = "[#000000]"  # Hitam
+M2 = "[#FF0000]"  # Merah
+H2 = "[#00FF00]"  # Hijau
+K2 = "[#FFFF00]"  # Kuning
+B2 = "[#00C8FF]"  # Biru
+U2 = "[#AF00FF]"  # Ungu
+N2 = "[#FF00FF]"  # Pink
+O2 = "[#00FFFF]"  # Biru Muda
+P2 = "[#FFFFFF]"  # Putih
+J2 = "[#FF8F00]"  # Jingga
+A2 = "[#AAAAAA]"  # Abu-Abu
+
 
 def encrypt_and_save_code(source_code, output_file):
-    encrypt = lambda data: base64.b64encode(zlib.compress(marshal.dumps(compile(data, "", "exec")))).decode("utf-8")
+    encrypt = lambda data: base64.b64encode(
+        zlib.compress(marshal.dumps(compile(data, "", "exec")))
+    ).decode("utf-8")
 
     encrypted_code = encrypt(source_code)
     current_time = datetime.datetime.now()
 
     with open(output_file, "w") as f:
-        f.write(f'__Author__ = "Ferly Afriliyan"\n__Method = "[( Marshal Zlib Base64 )]"\n__Github__ = "https://github.com/ferlyafriliyan"\n\n')
+        f.write(
+            f'__author__ = "Ryougaa Hideki"\n__madeBy__ = "[( Marshal Zlib Base64 )]"\n__git__ = "https://github.com/ferlyafriliyan"\n\n'
+        )
         f.write(f"# One of the best Python3.11 obfuscators.\n\n")
         f.write(f"# Di Buat Pada Tanggal : 15-8-2023\n\nimport base64, zlib, marshal\n")
-        f.write(f"uncompile6__=(marshal.loads(zlib.decompress(base64.b64decode(b'{encrypted_code}'))));exec(uncompile6__)")
+        f.write(
+            f"uncompile6__=(marshal.loads(zlib.decompress(base64.b64decode(b'{encrypted_code}'))));exec(uncompile6__)"
+        )
 
     print(f"{H2}[ {A2}• {H2}] Berhasil Obfuscated File - Hasil Encrypt : {output_file}")
 
-def custom_encrypt(input_file, output_file):
-    note = f'# Coded by : Ferly Afriliyan\n# Obfuscate use marshal, zlib, and base64\n# Do not share Original Files\n'   \
 
+def custom_encrypt(input_file, output_file):
+    note = f"# Coded by : Ryougaa Hideki\n# Do not share Original Files\n"
 
     try:
-        with open(input_file, 'r') as f:
+        with open(input_file, "r") as f:
             original_code = f.read()
     except IOError:
-        os.sys.exit('[!] File Not Found [!]')
+        os.sys.exit("[!] File Not Found [!]")
 
     fer4 = output_file
     ntaa = base64.b64encode(original_code.encode()).decode()
@@ -64,7 +71,7 @@ def custom_encrypt(input_file, output_file):
     h1 = []
     h2 = []
     h3 = []
-    h4 = ''
+    h4 = ""
 
     for x in ntaa:
         h1.append(ord(x))
@@ -87,27 +94,40 @@ def custom_encrypt(input_file, output_file):
     for s in h3:
         h4 += chr(s)
 
-    nta_xe = codecs.encode(h4, 'rot_13')
+    nta_xe = codecs.encode(h4, "rot_13")
 
     obf_note = note + "trust = '" + str(nta_xe) + "'\n"
 
-    h6 = open(fer4, 'w')
-    h6.write(obf_note + '\nimport codecs, base64\n' + 'magic = ' + str(h2) + '\t\t\n' + "love = '" + str(nta_xe) + "'\t\t\n")
+    h6 = open(fer4, "w")
+    h6.write(
+        obf_note
+        + "\nimport codecs, base64\n"
+        + "magic = "
+        + str(h2)
+        + "\t\t\n"
+        + "love = '"
+        + str(nta_xe)
+        + "'\t\t\n"
+    )
     h6.write("god = '\\x72\\x6f\\x74\\x5f\\x31\\x33'\n")
-    h6.write("destiny = codecs.decode(eval('\\x6c\\x6f\\x76\\x65'), eval('\\x67\\x6f\\x64'))\n")
-    h6.write("joy = base64.b64decode(''.join([chr(ferz) for ferz in magic])+eval('\\x64\\x65\\x73\\x74\\x69\\x6e\\x79'))\t\t\n")
+    h6.write(
+        "destiny = codecs.decode(eval('\\x6c\\x6f\\x76\\x65'), eval('\\x67\\x6f\\x64'))\n"
+    )
+    h6.write(
+        "joy = base64.b64decode(''.join([chr(ferz) for ferz in magic])+eval('\\x64\\x65\\x73\\x74\\x69\\x6e\\x79'))\t\t\n"
+    )
     h6.write("eval(compile(eval('\\x6a\\x6f\\x79'),'<string>','exec'))")
     h6.close()
 
     # Compile the encrypted file
     py_compile.compile(fer4)
 
-    print('[-] Successfully Encrypted and Compiled ' + fer4)
 
-if __name__ == '__main__':
-    os.system('clear')
+if __name__ == "__main__":
+    os.system("clear")
 
-    print(f"""
+    print(
+        f"""
 {B2}╔═╗┬  ┬┌─┐┬   ╔═╗┬ ┬  
 {H2}║╣ └┐┌┘├─┤│───╠═╝└┬┘  
 {P2}╚═╝ └┘ ┴ ┴┴─┘ ╩   ┴    Version : 0.2
@@ -115,11 +135,15 @@ if __name__ == '__main__':
 Author : Ferly Afriliyan
 Github : https:github.com/ferlyafriliyan
 -----------------------------------------
-""")
+"""
+    )
 
     file = input(f"{Putih}File : ")
     if not file:
-        print(f"{M2}[ {A2}• {M2}] {P2}Isi dengan benar {M2}!")
+        print(f" {M2}[{A2}!{M2}] {P2}File '{file}' tidak ditemukan.")
+        exit()
+    elif not file.endswith(".py"):
+        print(f" {M2}[{A2}!{M2}] {P2}File harus memiliki ekstensi .py")
         exit()
 
     if not os.path.isfile(file):
@@ -128,7 +152,10 @@ Github : https:github.com/ferlyafriliyan
 
     out = input(f"{Putih}Output : ")
     if not out:
-        out = "Enc_Marshal_py3.py"
+        out = f"codecs_{file}"
+    elif not out.endswith(".py"):
+        print(f" {M2}[{A2}!{M2}] {P2}File output harus memiliki ekstensi .py")
+        exit()
 
     try:
         source_code = open(file, "r", encoding="utf-8").read()
