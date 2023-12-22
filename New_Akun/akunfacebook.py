d7920ffe4e785901090692bf7375d30c93fd37f3
Ferly Afriliyan - Ryougaa <122451961+ferlyafriliyan@users.noreply.github.com>
2023-12-22 11:16:23 +0700

    Add files via upload

---
+++ New_Akun/akunfacebook.py
@@ -0,0 +1,51 @@
+import os as auto
+auto.system("clear")
+
+# Akun - 1
+Nama = "Denventa Denventa"
+Email = "elq83199@nezid.com"
+TTL = "04/04/2000"
+Pass = "Ferly4409_$XD"
+print("\n[•] Akun - 1\nNama FB :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun 2
+Nama = "Denventa Dev"
+Email = "cyr41869@omeie.com"
+TTL = "04/04/2000"
+Pass = "Ferly4409_$XD"
+print("\n[•] Akun - 2\nNama FB :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun 3
+Nama = "Denventa Dev"
+Email = "ber66338@zbok.com"
+TTL = "04/04/2000"
+Pass = "Ferly4409_$XD"
+print("\n[•] Akun - 3\nNama FB :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun 4 ( Pribadi )
+Nama = "Akari Nara ( Denventa )"
+Email = "deventa966@gmail.com"
+TTL = "10/Agustus/1910"
+Pass = "Ferly4409_$XD"
+print("\n[•] Akun - 4 ( Pribadi )\nNama FB :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun 5 ( Pribadi + Locked Profile )
+Nama = "Ryougaa Watashi"
+Email = "dvanmeplophferly@gmail.com"
+TTL = "26/Mei/1996"
+Pass = "DenventaAfriliyan费利4409#"
+print("\n[•] Akun - 5 ( Pribadi + Locked Profile)\nNama FB :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun 6
+Nama = "Ayu Novitasaridewi"
+Email = "giu91593@zslsz.com"
+TTL = "10/Maret/1995"
+Pass = "Ferly4409_$XD"
+print("\n[•] Akun - 6\nNama FB :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun 7
+Nama = "Ram Ram"
+Email = "ramadhamlff1718@gmail.com"
+TTL = "1/Oktober/2001"
+Pass = "DenventaAfriliyan费利4409#"
+print("\n[•] Akun - 7\nNama FB :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
