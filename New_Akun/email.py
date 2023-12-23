b381e3297cb49acff9840a334c5e1edd7456e416
Ferly Afriliyan - Ryougaa <122451961+ferlyafriliyan@users.noreply.github.com>
2023-12-23 15:00:31 +0700

    Add files via upload

--- New_Akun/email.py
+++ New_Akun/email.py
@@ -28,3 +28,39 @@ Email = "devenfer449@gmail.com"
 TTL = "04/04/2000"
 Pass = "Ferly4409#"
 print("\n[•] Akun - 4\nNama Email :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun - 5
+Nama = "Fitri Chan"
+Email = "fitrichan3055@gmail.com"
+TTL = "04/04/2004"
+Pass = "Ferly4409#"
+print("\n[•] Akun - 5\nNama Email :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun - 6
+Nama = "Azizi Shafaa Asadel"
+Email = "zeedenventa449@gmail.com"
+TTL = "04/04/2004"
+Pass = "Ferly4409#"
+print("\n[•] Akun - 6\nNama Email :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun - 7
+Nama = "Alexander Khontolatos"
+Email = "alexanderkhontolatos@gmail.com"
+TTL = "04/04/2000"
+Pass = "Ferly4409#"
+print("\n[•] Akun - 7\nNama Email :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun - 8
+Nama = "Alexander Khontolatos ll"
+Email = "alexanderkhontolatosll@gmail.com"
+TTL = "04/04/2000"
+Pass = "Ferly4409#"
+print("\n[•] Akun - 8\nNama Email :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
+# Akun - 9
+Nama = "New Akun"
+Email = "newakun4409@gmail.com"
+TTL = "04/04/2000"
+Pass = "Ferly4409#"
+print("\n[•] Akun - 9\nNama Email :", Nama + "\nEmail :", Email + "\nTtl :", TTL + "\nPassword :", Pass)
+
