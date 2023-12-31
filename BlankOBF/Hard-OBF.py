943441e6b970b83105c25e5f06b2902c88854c1e
Ferly Afriliyan - Ryougaa <122451961+ferlyafriliyan@users.noreply.github.com>
2023-12-31 12:46:56 +0700

    Add files via upload

---
+++ BlankOBF/Hard-OBF.py
@@ -0,0 +1,194 @@
+# If you want to use this in your project (with or without modifications, please give credits)
+
+import random
+import string
+import base64
+import codecs
+import os
+import sys
+from textwrap import wrap
+from lzma import compress
+from marshal import dumps
+
+
+def printerr(data):
+    print(data, file=sys.stderr)
+
+
+def clear():
+    os.system("cls" if "win" in sys.platform.lower() else "clear")
+
+
+class BlankOBF:
+    def __init__(self, code, outputpath):
+        self.code = code.encode()
+        self.outpath = outputpath
+        self.varlen = 3
+        self.vars = {}
+
+        self.marshal()
+        self.encrypt1()
+        self.encrypt2()
+        self.encrypt3()
+        self.finalize()
+
+    def generate(self, name):
+        res = self.vars.get(name)
+        if res is None:
+            res = "_" + "".join(["_" for _ in range(self.varlen)])
+            self.varlen += 1
+            self.vars[name] = res
+        return res
+
+    def encryptstring(self, string, config={}, func=False):
+        b64 = list(b"base64")
+        b64decode = list(b"b64decode")
+        __import__ = config.get("__import__", "__import__")
+        getattr = config.get("getattr", "getattr")
+        bytes = config.get("bytes", "bytes")
+        eval = config.get("eval", "eval")
+        if not func:
+            return f"{getattr}({__import__}({bytes}({b64}).decode()), {bytes}({b64decode}).decode())({bytes}({list(base64.b64encode(string.encode()))})).decode()"
+        else:
+            attrs = string.split(".")
+            base = self.encryptstring(attrs[0], config)
+            attrs = list(map(lambda x: self.encryptstring(x, config, False), attrs[1:]))
+            newattr = ""
+            for i, val in enumerate(attrs):
+                if i == 0:
+                    newattr = f"{getattr}({eval}({base}), {val})"
+                else:
+                    newattr = f"{getattr}({newattr}, {val})"
+            return newattr
+
+    def encryptor(self, config):
+        def func_(string, func=False):
+            return self.encryptstring(string, config, func)
+
+        return func_
+
+    def compress(self):
+        self.code = compress(self.code)
+
+    def marshal(self):
+        self.code = dumps(compile(self.code, "<string>", "exec"))
+
+    def encrypt1(self):
+        code = base64.b64encode(self.code).decode()
+        partlen = int(len(code) / 4)
+        code = wrap(code, partlen)
+        var1 = self.generate("a")
+        var2 = self.generate("b")
+        var3 = self.generate("c")
+        var4 = self.generate("d")
+        init = [
+            f'{var1}="{codecs.encode(code[0], "rot13")}"',
+            f'{var2}="{code[1]}"',
+            f'{var3}="{code[2][::-1]}"',
+            f'{var4}="{code[3]}"',
+        ]
+
+        random.shuffle(init)
+        init = ";".join(init)
+        self.code = f"""
+{init};
+__import__({self.encryptstring("builtins")}).exec(
+    __import__({self.encryptstring("marshal")}).loads(
+        __import__({self.encryptstring("base64")}).b64decode(
+            __import__({self.encryptstring("codecs")}).decode(
+                {var1}, 
+                __import__({self.encryptstring("base64")}).b64decode("{base64.b64encode(b'rot13').decode()}"
+                ).decode()
+            ) + {var2} + {var3}[::-1] + {var4}
+        )
+    )
+)
+""".strip().encode()
+
+    def encrypt2(self):
+        self.compress()
+        var1 = self.generate("e")
+        var2 = self.generate("f")
+        var3 = self.generate("g")
+        var4 = self.generate("h")
+        var5 = self.generate("i")
+        var6 = self.generate("j")
+        var7 = self.generate("k")
+        var8 = self.generate("l")
+        var9 = self.generate("m")
+
+        conf = {"getattr": var4, "eval": var3, "__import__": var8, "bytes": var9}
+        encryptstring = self.encryptor(conf)
+
+        self.code = f"""
+{var3} = eval({self.encryptstring("eval")});
+{var4} = {var3}({self.encryptstring("getattr")});
+{var8} = {var3}({self.encryptstring("__import__")});
+{var9} = {var3}({self.encryptstring("bytes")});
+{var5} = lambda {var7}: {var3}({encryptstring("compile")})(
+    {var7}, 
+    {encryptstring("<string>")}, 
+    {encryptstring("exec")}
+);
+{var1} = {self.code}
+{var2} = {encryptstring('__import__("builtins").list', func=True)}({var1})
+try:
+    {encryptstring('__import__("builtins").exec', func=True)}(
+        {var5}(
+            {encryptstring('__import__("lzma").decompress', func=True)}(
+                {var9}({var2})
+            )
+        )
+    ) or {encryptstring('__import__("os")._exit', func=True)}(0)
+except {encryptstring('__import__("lzma").LZMAError', func=True)}:
+    ...
+""".strip().encode()
+
+    def encrypt3(self):
+        self.compress()
+        data = base64.b64encode(self.code)
+        self.code = f"""
+from base64 import b64decode as _________
+import lzma as ____
+
+exec(
+    compile(
+        ____.decompress(
+            _________({data})
+        ), 
+        "<string>", 
+        "exec"
+    )
+)
+""".encode()
+
+    def finalize(self):
+        if os.path.dirname(self.outpath).strip() != "":
+            os.makedirs(os.path.dirname(self.outpath), exist_ok=True)
+        with open(self.outpath, "w") as e:
+            e.write(self.code.decode())
+        print("Saved as --> " + os.path.realpath(self.outpath))
+
+
+if __name__ == "__main__":
+    try:
+        clear()
+        sourcefile = input("Input file to Obfuscate: ")
+
+        if not os.path.isfile(sourcefile):
+            printerr(f'No such file: "{sourcefile}"')
+            sys.exit(1)
+        elif not sourcefile.endswith((".py", ".pyw")):
+            printerr("The file does not have a valid python script extension!")
+            sys.exit(1)
+
+        outputfile = input('output file path [Default: "Obfuscated_<FILE>.py"]: ')
+        if not outputfile:
+            outputfile = "Obfuscated_" + os.path.basename(sourcefile)
+
+        with open(sourcefile) as sourcefile:
+            code = sourcefile.read()
+
+        BlankOBF(code, outputfile)
+    except (KeyboardInterrupt, Exception) as e:
+        exit(f"[Error]: {str(e).capitalize()} !")
