54a0ec545c5e8d9fe82d4dd8381390aee0941336
Ferly Afriliyan - Ryougaa <122451961+ferlyafriliyan@users.noreply.github.com>
2024-01-22 23:24:43 +0700

    Add files via upload

---
+++ InstaVipRozhak/hitung.py
@@ -0,0 +1,11 @@
+# Jumlah koin yang dimiliki
+total_koin = 10600
+
+# Harga satu followers dalam koin
+harga_per_followers = 500
+
+# Menghitung jumlah followers yang dapat dibeli
+jumlah_followers = total_koin // harga_per_followers
+
+# Menampilkan hasil
+print("Jumlah followers yang dapat Anda beli:", jumlah_followers)
