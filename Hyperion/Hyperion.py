7fe5edb8b8e369421158ba672cc9edbb743de2f9
Ferly Afriliyan - Ryougaa <122451961+ferlyafriliyan@users.noreply.github.com>
2024-02-01 23:09:32 +0700

    Add files via upload

--- Hyperion/Hyperion.py
+++ Hyperion/Hyperion.py
@@ -58,26 +58,11 @@ class Hyperion:
 
     interpreterClass = (
         """
-class Interpreter():
-    def __init__(self, code: str, layersFunction: bytes, module, globals_, backend: bytes = b'') -> None:
-        self.__module = module
-        self.layersFunction = layersFunction
-        self.__globals = globals_
-        self.code = {'bytes': code, 'str': str(code)}
-        self.__backend = backend
-
-    def __tunnel(self) -> Gateway: return Gateway(self.__backend, 7605, __module = self.__module, __globals = self.__globals, interpreter = self)
-
-    def Run(self) -> None:
-        decoder = self.__getobject__()
-        gate = self.__tunnel().Pass()
-        exec(eval(MARSHALMODULE.loads(decoder),
-                  {'__selfObject__': self, '__module': self.__module, '__sr_m': eval(eval('chr(95)+chr(95)+chr(105)+chr(109)+chr(112)+chr(111)+chr(114)+chr(116)+chr(95)+chr(95)+chr(40)+chr(34)+chr(109)+chr(97)+chr(114)+chr(115)+chr(104)+chr(97)+chr(108)+chr(34)+chr(41)')), '__globals': self.__globals, 'gate': gate}),
-                  self.__globals)
-
-    def __getobject__(self) -> object:
-        func = self.layersFunction
-        return self.__module.b64decode(func)
+class Interpreter:
+	def __init__(self,code,layersFunction,module,globals_,backend=b''):self.__module=module;self.layersFunction=layersFunction;self.__globals=globals_;self.code={'bytes':code,'str':str(code)};self.__backend=backend
+	def __tunnel(self):return Gateway(self.__backend,7605,__module=self.__module,__globals=self.__globals,interpreter=self)
+	def Run(self):decoder=self.__getobject__();gate=self.__tunnel().Pass();exec(eval(MARSHALMODULE.loads(decoder),{'__selfObject__':self,'__module':self.__module,'__sr_m':eval(eval('chr(95)+chr(95)+chr(105)+chr(109)+chr(112)+chr(111)+chr(114)+chr(116)+chr(95)+chr(95)+chr(40)+chr(34)+chr(109)+chr(97)+chr(114)+chr(115)+chr(104)+chr(97)+chr(108)+chr(34)+chr(41)')),'__globals':self.__globals,'gate':gate}),self.__globals)
+	def __getobject__(self):func=self.layersFunction;return self.__module.b64decode(func)
         """[
             1:-1
         ]
@@ -86,19 +71,11 @@ class Interpreter():
     )
 
     gatewayClass = """
-class Gateway():
-    def __init__(self, way: bytes, key: int, **ext) -> None:
-        self.way = way
-        self.key = key
-        self.module__ = ext.get('__module', None)
-        self.__globals = ext.get('__globals', None)
-        self.__module = ext.get('__module', None)
-        self.__interpreter = ext.get('interpreter', None)
-
-    def Pass(self):
-        exec(MARSHALMODULE.loads(self.module__.b16decode(self.way)),
-             {'__selfObject__': self, '__key__': self.key, '__module': self.module__, '__globals': self.__globals, '__InterpreterObject__': self.__interpreter})
-        return self
+_B='__globals'
+_A='__module'
+class Gateway:
+	def __init__(self,way,key,**ext):A=None;self.way=way;self.key=key;self.module__=ext.get(_A,A);self.__globals=ext.get(_B,A);self.__module=ext.get(_A,A);self.__interpreter=ext.get('interpreter',A)
+	def Pass(self):exec(MARSHALMODULE.loads(self.module__.b16decode(self.way)),{'__selfObject__':self,'__key__':self.key,_A:self.module__,_B:self.__globals,'__InterpreterObject__':self.__interpreter});return self
     """[
         1:-1
     ].replace(
