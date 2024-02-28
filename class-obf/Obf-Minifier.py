b9613bc483d8438c0a1ce0ce31ded48608dc3e1c
Ferly Afriliyan - Ryougaa <122451961+ferlyafriliyan@users.noreply.github.com>
2024-02-28 15:38:41 +0700

    Update Obf-Minifier.py

--- class-obf/Obf-Minifier.py
+++ class-obf/Obf-Minifier.py
@@ -14,7 +14,7 @@ def Banner():os.system('cls'if'win'in sys.platform.lower()else'clear');print(f""
 def obfuscate_and_save():
 	B='exec';A='';C=read_file();D=compile(C,A,B);E=marshal.dumps(D);F=zlib.compress(E);G=bz2.compress(F);H=lzma.compress(G);I=f"""#i/urs/bin/python3.11
 try:
-\t__Minh_x_Uyen__=locals();__Botname__='@MinhNguyen_x_NgocUyen_bot';__Date_Obf__='2024-02-07 - Admin (UTC)';__Mode_ENC__='3.11.6 -> (main) - version: 1.0.1';__OBFUSCATION_BY__='MinhNguyen2412_x_NgocUyen1907'
+\t__Minh_x_Uyen__=locals();__Botname__='@ChatGPT_bot';__Date_Obf__='2024-02-07 - Admin (UTC)';__Mode_ENC__='3.11.6 -> (main) - version: 1.0.2';__OBFUSCATION_BY__='MinhNguyen2412_x_NgocUyen1907'
 \tclass PyObject:
 \t\tdef PythonCodeObject(code):return code*2
 \t\tdef Obfuscator(code_,_code):__Minh_x_Uyen__[code_]=_code;return __Minh_x_Uyen__[code_]
@@ -41,9 +41,23 @@ try:
 \t\t\treturn b''.join(code_)
 \t__CodeObjectData__=PyObject.PythonCodeObject(__import__('math').floor(5)),PyObject.Obfuscator('__CodeObjectData__',{H});__import__('builtins').eval(__import__('marshal').loads(__import__('zlib').decompress(PyObject.Windows(PyObject.KaliLinux(__CodeObjectData__[__import__('math').floor(1)])))))
 except MemoryError:print('MemoryError: >> GOOD LUCK!! , OBF by [NgocUyen x MinhNguyen] <<')
-except Exception as e:__import__('logging').error(__import__('traceback').format_exc())""";J=marshal.dumps(compile(I.encode(),A,B));K=bz2.compress(J);L=f"#i/usr/bin/python3.11\ntry:exec(__import__('marshal').loads(__import__('bz2').BZ2Decompressor().decompress({K})),globals())\nexcept KeyboardInterrupt:print();__import__('sys').exit()\nexcept Exception as e:__import__('logging').error(__import__('traceback').format_exc())";M=marshal.dumps(compile(L.encode(),A,B));N=zlib.compress(M);O=base64.b64encode(N).decode();P=f"#i/urs/bin/python3.11\ntry:exec(__import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode({repr(O)}))),globals())\nexcept KeyboardInterrupt:print();__import__('sys').exit()\nexcept Exception as e:__import__('logging').error(__import__('traceback').format_exc())";Q=marshal.dumps(compile(P.encode(),A,B));R=lzma.compress(Q);S=bz2.compress(R);T=base64.b64encode(S);U=f"#i/usr/bin/python3.11\ntry:exec(__import__('marshal').loads(__import__('lzma').LZMADecompressor().decompress(__import__('bz2').BZ2Decompressor().decompress(__import__('base64').b64decode({repr(T)})))),globals())\nexcept KeyboardInterrupt:print();__import__('sys').exit()\nexcept Exception as e:__import__('logging').error(__import__('traceback').format_exc())";V=marshal.dumps(compile(U.encode(),A,B));W=zlib.compress(V);X=bz2.compress(W);Y=lzma.compress(X);Z=f"""#i/urs/bin/python3.11
+except Exception as e:__import__('logging').error(__import__('traceback').format_exc())""";J=marshal.dumps(compile(I.encode(),A,B));K=bz2.compress(J);L=f"#i/usr/bin/python3.11\ntry:exec(__import__('marshal').loads(__import__('bz2').BZ2Decompressor().decompress({K})),globals())\nexcept KeyboardInterrupt:print();__import__('sys').exit()\nexcept Exception as e:__import__('logging').error(__import__('traceback').format_exc())";M=marshal.dumps(compile(L.encode(),A,B));N=zlib.compress(M);O=base64.b64encode(N).decode();P=f"#i/urs/bin/python3.11\ntry:exec(__import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode({repr(O)}))),globals())\nexcept KeyboardInterrupt:print();__import__('sys').exit()\nexcept Exception as e:__import__('logging').error(__import__('traceback').format_exc())";Q=marshal.dumps(compile(P.encode(),A,B));R=lzma.compress(Q);S=bz2.compress(R);T=base64.b64encode(S);U=f"#i/usr/bin/python3.11\ntry:exec(__import__('marshal').loads(__import__('lzma').LZMADecompressor().decompress(__import__('bz2').BZ2Decompressor().decompress(__import__('base64').b64decode({repr(T)})))),globals())\nexcept KeyboardInterrupt:print();__import__('sys').exit()\nexcept Exception as e:__import__('logging').error(__import__('traceback').format_exc())";V=U.encode();W=compile(V,A,B);X=marshal.dumps(W);Y=bz2.compress(X);Z=base64.b32hexencode(Y);a=zlib.compress(Z,level=zlib.Z_BEST_COMPRESSION);b=f"""
+
 try:
-\t__Minh_x_Uyen__=locals();__Botname__='@MinhNguyen_x_NgocUyen_bot';__Date_Obf__='2024-02-07 - Admin (UTC)';__Mode_ENC__='3.11.6 -> (main) - version: 1.0.1';__OBFUSCATION_BY__='MinhNguyen2412_x_NgocUyen1907'
+    if OBFUSCATED_BY != '@Ferly Afriliyan':
+        int('skid')
+except:
+    input(\"Gak usah Direcode Goblok !\")
+    __import__('sys').exit()
+
+""";c=compile(b,A,B);d=marshal.dumps(c);e=bz2.compress(d);f=base64.b32hexencode(e);g=zlib.compress(f,level=zlib.Z_BEST_COMPRESSION);h=f"\neval(__import__('marshal').loads(__import__('_bz2').BZ2Decompressor().decompress(__import__('base64').b32hexdecode(__import__('zlib').decompress({repr(g)})))), globals())";i=f"""
+OBFUSCATED_BY = '@Ferly Afriliyan'
+# try:
+# \tif OBFUSCATED_BY!='@Ferly Afriliyan':int('skid')
+# except:input('Gak usah Direcode Goblok !');__import__('sys').exit()
+{h};eval(__import__('marshal').loads(__import__('_bz2').BZ2Decompressor().decompress(__import__('base64').b32hexdecode(__import__('zlib').decompress({repr(a)})))), globals())""";j=marshal.dumps(compile(i.encode(),A,B));k=zlib.compress(j);l=bz2.compress(k);m=lzma.compress(l);n=f"""#i/urs/bin/python3.11
+try:
+\t__Minh_x_Uyen__=locals();__Botname__='@ChatGPT_bot';__Date_Obf__='2024-02-07 - Admin (UTC)';__Mode_ENC__='3.11.6 -> (main) - version: 1.0.2';__OBFUSCATION_BY__='MinhNguyen2412_x_NgocUyen1907'
 \tclass PyObject:
 \t\tdef PythonCodeObject(code):return code*2
 \t\tdef Obfuscator(code_,_code):__Minh_x_Uyen__[code_]=_code;return __Minh_x_Uyen__[code_]
@@ -68,10 +82,10 @@ try:
 \t\t\t\tcode_.append(_code);code=Ngoc_Uyen.unused_data
 \t\t\t\tif not code:break
 \t\t\treturn b''.join(code_)
-\t__CodeObjectData__=PyObject.PythonCodeObject(__import__('math').floor(5)),PyObject.Obfuscator('__CodeObjectData__',{Y});__import__('builtins').eval(__import__('marshal').loads(__import__('zlib').decompress(PyObject.Windows(PyObject.KaliLinux(__CodeObjectData__[__import__('math').floor(1)])))))
+\t__CodeObjectData__=PyObject.PythonCodeObject(__import__('math').floor(5)),PyObject.Obfuscator('__CodeObjectData__',{m});__import__('builtins').eval(__import__('marshal').loads(__import__('zlib').decompress(PyObject.Windows(PyObject.KaliLinux(__CodeObjectData__[__import__('math').floor(1)])))))
 except MemoryError:print('MemoryError: >> GOOD LUCK!! , OBF by [NgocUyen x MinhNguyen] <<')
-except Exception as e:__import__('logging').error(__import__('traceback').format_exc())""";a=input('Output File: ')
-	with open(a,'w')as b:b.write(Z)
+except Exception as e:__import__('logging').error(__import__('traceback').format_exc())""";o=input('Output File: ')
+	with open(o,'w')as p:p.write(n)
 if __name__=='__main__':
 	try:Banner();obfuscate_and_save()
 	except KeyboardInterrupt:print();__import__('sys').exit()
